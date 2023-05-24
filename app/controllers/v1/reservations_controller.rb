709105158e545a92478aba472360d66ed5cc33c0
mtaufan.dev <mtaufan.dev@gmail.com>
2023-05-24 12:43:15 +0700

    add simple parsing for guest and reservation creation

--- app/controllers/v1/reservations_controller.rb
+++ app/controllers/v1/reservations_controller.rb
@@ -4,9 +4,18 @@ module V1
   class ReservationsController < ApplicationController
     def create_reservation
       parsed_payload = parse_payload(params)
-      response = create_booking(parsed_payload)
-      if response
-        render json: { message: 'Booking created', data: response }, status: :created
+
+      guest = Guest.find_or_create_by!(email: guest_params[:email]) do |data|
+        data.firstname = guest_params[:firstname]
+        data.lastname = guest_params[:lastname]
+        data.phone = guest_params[:phone]
+      end
+
+      reservation = guest.reservations.find_or_initialize_by(reservation_code: params['reservation_code'])
+      reservation.update!(reservation_params.except(:email))
+
+      if reservation
+        render json: { message: 'Booking created', data: reservation }, status: :created
       else
         render json: { error: 'Partner not found' }, status: :bad_request
       end
@@ -19,13 +28,38 @@ module V1
         { booking_code: params['reservation_code'], partner: 'Airbnb' }
       elsif params.key?('reservation') && params['reservation']['code'].start_with?('XXX')
         { booking_code: params['reservation']['code'], partner: 'Airbnc' }
-      else
-        nil
       end
     end
 
     def create_booking(parsed_payload)
       parsed_payload
+    end
+
+    def guest_params
+      {
+        firstname: params['guest']['first_name'],
+        lastname: params['guest']['last_name'],
+        phone: params['guest']['phone'],
+        email: params['guest']['email']
+      }
+    end
+
+    def reservation_params
+      {
+        reservation_code: params['reservation_code'],
+        start_date: Date.parse(params['start_date']),
+        end_date: Date.parse(params['end_date']),
+        night_count: params['nights'],
+        guest_count: params['guests'],
+        children_count: params['children'],
+        infant_count: params['infants'],
+        adult_count: params['adults'],
+        status: params['status'],
+        currency: params['currency'],
+        payout_price: params['payout_price'],
+        security_price: params['security_price'],
+        total_price: params['total_price']
+      }
     end
   end
 end
